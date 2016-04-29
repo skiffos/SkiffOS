ca3d15fe50a62809fb450a9c082c0ece53593017
Christian Stewart <christian@paral.in>
2016-04-29 04:42:46 -0400

    Minor tweaks to help

--- scripts/print_help.sh
+++ scripts/print_help.sh
@@ -6,13 +6,12 @@ echo -e "\e[7;49;31m"
 cat ../resources/text/logo.ascii
 echo -e "\e[0m"
 
-if [ -n "$SKIFF_CONFIG" ]; then
-  echo ""
-  if ERR=$(../scripts/verify_selected_config.sh 2>&1); then
-    printf "\033[0;34m✓ Selected config:\033[0m $SKIFF_CONFIG\n"
-  else
-    printf "\033[1;49;31m✖ $ERR\033[0m\n"
-  fi
+echo ""
+if ERR=$(../scripts/verify_selected_config.sh 2>&1); then
+  printf "\033[0;34m✓ Selected config:\033[0m $SKIFF_CONFIG\n"
+  printf "  $(cat $SKIFF_CONFIG_PATH/$SKIFF_CONFIG_METADATA_SUBDIR/$SKIFF_CONFIG_METADATA_DESCRIPTION)\n"
+else
+  printf "\033[1;49;31m✖ $ERR\033[0m\n"
 fi
 
 echo ""
