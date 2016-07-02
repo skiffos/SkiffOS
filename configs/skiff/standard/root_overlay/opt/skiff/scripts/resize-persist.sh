639e68a64702bfa59accbb7c9bf870e6f91edccd
Christian Stewart <christian@paral.in>
2016-07-02 02:40:20 -0700

    Resize script working properly

--- configs/skiff/standard/root_overlay/opt/skiff/scripts/resize-persist.sh
+++ configs/skiff/standard/root_overlay/opt/skiff/scripts/resize-persist.sh
@@ -18,19 +18,21 @@ fi
 echo "Disk partition detected at ${MOUNTPOINT} -> ${DEVPATH}"
 disk=$(lsblk -no pkname $DEVPATH)
 echo "Disk detected at ${disk}."
-part_num=$(lsblk -f $DEVPATH -o "MAJ:MIN" | tail -n1 | cut -d: -f2)
+part_num=$(lsblk -f $DEVPATH -o "MAJ:MIN" | tail -n1 | cut -d: -f2 | tr -d '[[:space:]]')
 echo "Partition number detected as $part_num"
 disk_part=$DEVPATH
-p2_start=$(fdisk -l /dev/mmcblk0 | grep $disk_part | awk '{print $2}')
+p2_start=$(fdisk -l /dev/mmcblk0 | grep $disk_part | awk '{print $2}' | tr -d '[[:space:]]')
 echo "Partition start: $p2_start"
-p2_end=$(fdisk -l /dev/mmcblk0 | grep $disk_part | awk '{print $3}')
+p2_end=$(fdisk -l /dev/mmcblk0 | grep $disk_part | awk '{print $3}' | tr -d '[[:space:]]')
 echo "Partition end: $p2_end"
 
 disk_size=$(blockdev --getsize /dev/$disk)
 echo "Disk size: $disk_size"
 
-if [ ! $(($disk_size-$p2_end)) -le 10485760 ]; then
-  echo "No need to resize, exiting."
+if [ ! $(($disk_size-$p2_end)) -ge 10485760 ]; then
+  echo "No need to resize physical, exiting."
+  resize2fs ${disk_part} || true
+  touch $MARKER
   exit 0
 fi
 
@@ -38,6 +40,7 @@ p2_new_end=$((disk_size-10))
 
 echo "Resizing $disk_part from $p2_end to $p2_new_end"
 
+set +e
 fdisk /dev/mmcblk0 <<EOF
 p
 d
@@ -51,7 +54,5 @@ p
 w
 EOF
 
-resize2fs ${disk_part}
-touch $MARKER
-echo "Resized rootfs, rebooting"
+echo "Resized persist. Rebooting."
 reboot
