02900d6ed0889d29d16a8f5c7e9ed027d31839f3
Christian Stewart <christian@paral.in>
2017-04-12 14:24:39 -0400

    fix: set buildroot_dir correctly

--- scripts/primary_env.sh
+++ scripts/primary_env.sh
@@ -10,9 +10,9 @@ source ../scripts/check_workspace.sh
 source ../scripts/maybe_recover_skiff_config.sh
 source ../scripts/enumerate_configs.sh >/dev/null
 export BUILDROOT_DEFAULT_DIR=${ROOT_DIR}/buildroot
-export BUILDROOT_DIR=${ROOT_DIR}/buildroot
-export SKIFF_BUILDROOT_DIR=${ROOT_DIR}/workspaces/${SKIFF_WORKSPACE}
-export SKIFF_WORKSPACE_DIR=${ROOT_DIR}/workspaces/${SKIFF_WORKSPACE}
 export WORKSPACE_DIR=${ROOT_DIR}/workspaces/${SKIFF_WORKSPACE}
+export BUILDROOT_DIR=${WORKSPACE_DIR}
+export SKIFF_BUILDROOT_DIR=${WORKSPACE_DIR}
+export SKIFF_WORKSPACE_DIR=${WORKSPACE_DIR}
 cd ../build
 make $@
