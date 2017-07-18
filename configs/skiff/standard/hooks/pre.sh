8117fd7b5e6b58392364aa649d996d0acdc7ffd2
Christian Stewart <christian@paral.in>
2017-07-18 16:29:43 -0700

    fix: remove resolv.conf simlink to systemd-resolved

--- configs/skiff/standard/hooks/pre.sh
+++ configs/skiff/standard/hooks/pre.sh
@@ -2,4 +2,5 @@
 
 mkdir -p ${SKIFF_BUILDROOT_DIR}/output/images/resources
 rm ${SKIFF_BUILDROOT_DIR}/output/target/usr/lib/systemd/system/network-online.target.wants/NetworkManager-wait-online.service || true
+rm ${SKIFF_BUILDROOT_DIR}/output/target/etc/resolv.conf || true
 echo "# Managed by NetworkManager" > ${SKIFF_BUILDROOT_DIR}/output/target/etc/resolv.conf
