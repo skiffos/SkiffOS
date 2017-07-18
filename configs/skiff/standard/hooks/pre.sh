75e41e1457f9a4273ba39ba8ddab6091317084ec
Christian Stewart <christian@paral.in>
2017-07-18 16:06:47 -0700

    fix: correct path to NetworkManager-wait-online

--- configs/skiff/standard/hooks/pre.sh
+++ configs/skiff/standard/hooks/pre.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p ${SKIFF_BUILDROOT_DIR}/output/images/resources
-rm ${SKIFF_BUILDROOT_DIR}/output/target/usr/lib/systemd/system/network-online.target.wants || true
+rm ${SKIFF_BUILDROOT_DIR}/output/target/usr/lib/systemd/system/network-online.target.wants/NetworkManager-wait-online.service || true
