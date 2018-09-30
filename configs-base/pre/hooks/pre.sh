137903f757f3ea24ec1b1d391217ba71ee77b3f6
Christian Stewart <christian@paral.in>
2018-09-29 17:29:15 -0700

    base: add SKIFF_CONFIG to the skiff-version
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs-base/pre/hooks/pre.sh
+++ configs-base/pre/hooks/pre.sh
@@ -11,7 +11,8 @@ mkdir -p ${TARGET_DIR}/etc
   echo "PRETTY_NAME=\"SkiffOS ${SKIFF_VERSION}\""; \
   echo "BUILD_DATE=\"$(date)\""; \
   echo "BUILD_USER=\"${USER}@$(hostname)\""; \
-  echo "VERSION_FULL=\"SkiffOS version ${SKIFF_VERSION} (${USER}@$(hostname)) $(date)"; \
+  echo "VERSION_FULL=\"SkiffOS version ${SKIFF_VERSION} (${USER}@$(hostname)) $(date)\""; \
+  echo "BUILD_CONFIG=\"${SKIFF_CONFIG}\""; \
   ) > ${TARGET_DIR}/etc/skiff-release
 
 if [ ! -f ${TARGET_DIR}/etc/buildroot-release ]; then
