b147419f97385ba8e42b98053b0fcdd677624e17
Christian Stewart <christian@paral.in>
2019-11-17 04:03:16 -0800

    skiff/core: update to 1.3.0, default to no scratchbuild
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/skiff/core/buildroot_ext/package/skiff-core/skiff-core.mk
+++ configs/skiff/core/buildroot_ext/package/skiff-core/skiff-core.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKIFF_CORE_VERSION = v1.2.0
+SKIFF_CORE_VERSION = v1.3.0
 SKIFF_CORE_SITE = $(call github,paralin,skiff-core,$(SKIFF_CORE_VERSION))
 SKIFF_CORE_LICENSE = MIT
 SKIFF_CORE_LICENSE_FILES = LICENSE
