fea3f68f8fcebea9f0425a1b4acec5c200c9300d
Christian Stewart <christian@paral.in>
2020-01-11 16:22:10 -0800

    {pi,bananapi}/common: use a mounts directory inside output
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/pi/common/scripts/install_sd.sh
+++ configs/pi/common/scripts/install_sd.sh
@@ -39,7 +39,10 @@ if [ ! -f "$mkknlimg" ]; then
 fi
 
 mounts=()
-WORK_DIR=`mktemp -d -p "$DIR"`
+MOUNTS_DIR=${outp_path}/mounts
+mkdir -p ${MOUNTS_DIR}
+WORK_DIR=`mktemp -d -p "${MOUNTS_DIR}"`
+
 # deletes the temp directory
 function cleanup {
 sync || true
