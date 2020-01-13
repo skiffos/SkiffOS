9262af4fb441f6cded16cfbe7f8681afc68f3018
Christian Stewart <christian@paral.in>
2020-01-13 01:53:58 -0800

    skiff/standard: use output directory without symlink
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/skiff/standard/hooks/pre.sh
+++ configs/skiff/standard/hooks/pre.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-TARGET_DIR=${SKIFF_BUILDROOT_DIR}/output/target
+TARGET_DIR=${SKIFF_BUILDROOT_DIR}/target
 SYSTEMD_DIR=${TARGET_DIR}/usr/lib/systemd/system
 
-mkdir -p ${SKIFF_BUILDROOT_DIR}/output/images/resources
+mkdir -p ${SKIFF_BUILDROOT_DIR}/images/resources
 rm ${SYSTEMD_DIR}/network-online.target.wants/NetworkManager-wait-online.service || true
 mkdir -p ${SYSTEMD_DIR}/basic.target.wants
 if [ -f ${SYSTEMD_DIR}/sysinit.target.wants/systemd-journald.service ]; then
     mv ${SYSTEMD_DIR}/sysinit.target.wants/systemd-journal* ${SYSTEMD_DIR}/basic.target.wants/ || true
 fi
-rm ${SKIFF_BUILDROOT_DIR}/output/target/etc/resolv.conf || true
-echo "# Managed by NetworkManager" > ${SKIFF_BUILDROOT_DIR}/output/target/etc/resolv.conf
+rm ${SKIFF_BUILDROOT_DIR}/target/etc/resolv.conf || true
+echo "# Managed by NetworkManager" > ${SKIFF_BUILDROOT_DIR}/target/etc/resolv.conf
