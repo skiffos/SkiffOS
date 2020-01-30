2d9b67413d21b1a29d724621006250be8d8b07e6
Christian Stewart <christian@paral.in>
2020-01-30 23:52:33 +0000

    virt/virtualbox: fix image build scripts
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/virt/virtualbox/scripts/build_image.sh
+++ configs/virt/virtualbox/scripts/build_image.sh
@@ -5,6 +5,7 @@ IMAGES_DIR=$BUILDROOT_DIR/output/images
 SYS_IMAGE_DIR=${IMAGES_DIR}/sys-image
 ROOTFS_DISK=${SYS_IMAGE_DIR}/image-rootfs.img
 
+mkdir -p ${SYS_IMAGE_DIR}
 qemu-img convert \
 	-f raw \
  	-O vmdk \
