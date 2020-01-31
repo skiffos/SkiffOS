169e0cbc1a0a64e36bf1d77b942b6155691f1a2f
Christian Stewart <christian@paral.in>
2020-01-31 02:15:38 +0000

    virt/virtualbox: install grub to target and copy config
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/intel/x64/hooks/post.sh
+++ configs/intel/x64/hooks/post.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Building target filesystem image..."
+echo "Copying grub configuration to target..."
 cp ${SKIFF_CURRENT_CONF_DIR}/resources/grub/grub.cfg \
-	${SKIFF_BUILDROOT_DIR}/output/images/efi-part/EFI/BOOT/grub.cfg
+	 ${SKIFF_BUILDROOT_DIR}/output/images/grub.cfg
+
+echo "Building target filesystem image..."
 ${SKIFF_CURRENT_CONF_DIR}/scripts/build_image.sh
 
