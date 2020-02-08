5855636555dbaafc8aa55991f6438da789794c97
Christian Stewart <christian@paral.in>
2020-02-08 14:42:34 -0800

    intel/x64: remove image build hook and add command, use efi
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/intel/x64/hooks/post.sh
+++ configs/intel/x64/hooks/post.sh
@@ -4,6 +4,3 @@ echo "Copying grub configuration to target..."
 cp ${SKIFF_CURRENT_CONF_DIR}/resources/grub/grub.cfg \
 	 ${SKIFF_BUILDROOT_DIR}/output/images/grub.cfg
 
-echo "Building target filesystem image..."
-${SKIFF_CURRENT_CONF_DIR}/scripts/build_image.sh
-
