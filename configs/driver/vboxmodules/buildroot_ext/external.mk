3e8dca0de2bb0a9057197a6e1386511fdec9df69
Christian Stewart <christian@paral.in>
2020-03-01 00:03:34 -0800

    driver/vboxmodules: new configuration package
    Includes a buildroot external package for the VirtualBox kernel modules, derived
    from the Gentoo build files.
    
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ configs/driver/vboxmodules/buildroot_ext/external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_DRIVER_VBOXMODULES_PATH)/package/*/*.mk))
---
+++ configs/driver/vboxmodules/buildroot_ext/package/virtualbox-modules/virtualbox-modules.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# virtualbox-modules
+#
+################################################################################
+
+VIRTUALBOX_MODULES_VERSION = 6.1.4
+VIRTUALBOX_MODULES_SITE = https://dev.gentoo.org/~polynomial-c/virtualbox
+VIRTUALBOX_MODULES_SOURCE = vbox-kernel-module-src-$(VIRTUALBOX_MODULES_VERSION).tar.xz
+VIRTUALBOX_MODULES_LICENSE = GPL-2
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
