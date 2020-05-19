312c3d994d4049b2159c56600832163ac4aef249
Christian Stewart <christian@paral.in>
2020-05-18 21:39:07 -0700

    driver/wl: bump hash to latest version
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/driver/wl/buildroot_ext/package/broadcom-sta/broadcom-sta.mk
+++ configs/driver/wl/buildroot_ext/package/broadcom-sta/broadcom-sta.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BROADCOM_STA_VERSION = c393e8aa87d286708bcb868549bcb2013391c5f7
+BROADCOM_STA_VERSION = a9362f4e4416d8e842b976fd243445711363ec24
 BROADCOM_STA_SITE = $(call github,antoineco,broadcom-wl,$(BROADCOM_STA_VERSION))
 LICENSE = Proprietary
 
