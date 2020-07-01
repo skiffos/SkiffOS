401cb78872cca75a8d74a5fbc0328058eb585b69
Christian Stewart <christian@paral.in>
2020-06-30 23:34:21 -0700

    fix: always symlink output to workspace dir
    Signed-off-by: Christian Stewart <christian@paral.in>

--- scripts/setup_workspace.sh
+++ scripts/setup_workspace.sh
@@ -5,10 +5,13 @@ if [ ! -d "$WORKSPACE_DIR" ] || [ ! -f "$WORKSPACE_DIR/Makefile" ]; then
   # Setup the worktree
   cd $BUILDROOT_DEFAULT_DIR
   make O=$WORKSPACE_DIR defconfig
-  ln -f -s $WORKSPACE_DIR $WORKSPACE_DIR/output
   cd - > /dev/null
 fi
 if [ ! -d "$SKIFF_WS_OVERRIDES_DIR" ]; then
     mkdir -p $SKIFF_WS_OVERRIDES_DIR
     echo "Place Skiff configuration files for the $SKIFF_WORKSPACE workspace here." > $SKIFF_WS_OVERRIDES_DIR/README
 fi
+
+# Ensure that output symbolic links to the same folder.
+ln -f -s $WORKSPACE_DIR $WORKSPACE_DIR/output
+
