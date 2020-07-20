9929113bcc4e72485b87a7959a6bb9c55d9d4bc3
Christian Stewart <christian@paral.in>
2020-07-19 21:06:43 -0700

    nvidia/tegra: cleanup skiff-core-linux4tegra package
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/nvidia/tegra/buildroot_ext/package/skiff-core-linux4tegra/skiff-core-linux4tegra.mk
+++ configs/nvidia/tegra/buildroot_ext/package/skiff-core-linux4tegra/skiff-core-linux4tegra.mk
@@ -7,11 +7,11 @@
 SKIFF_CORE_LINUX4TEGRA_DEPENDENCIES = skiff-core
 
 define SKIFF_CORE_LINUX4TEGRA_INSTALL_COREENV
-	mkdir -p $(TARGET_DIR)/opt/skiff/coreenv
-	$(INSTALL) -m 0644 $(SKIFF_CORE_LINUX4TEGRA_PKGDIR)/coreenv-defconfig.yaml \
-		$(TARGET_DIR)/opt/skiff/coreenv/defconfig.yaml
 	mkdir -p $(TARGET_DIR)/opt/skiff/coreenv/skiff-core-linux4tegra
 	cp -r $(SKIFF_CORE_LINUX4TEGRA_PKGDIR)/coreenv/* $(TARGET_DIR)/opt/skiff/coreenv/skiff-core-linux4tegra/
+	$(INSTALL) -m 0644 $(SKIFF_CORE_LINUX4TEGRA_PKGDIR)/coreenv-defconfig.yaml \
+		$(TARGET_DIR)/opt/skiff/coreenv/defconfig.yaml
+	touch $(TARGET_DIR)/opt/skiff/coreenv/.overridden
 endef
 
 SKIFF_CORE_LINUX4TEGRA_FINALIZE_HOOKS += SKIFF_CORE_LINUX4TEGRA_INSTALL_COREENV
