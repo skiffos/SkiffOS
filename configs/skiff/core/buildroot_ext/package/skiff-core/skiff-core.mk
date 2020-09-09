6966e4d4eb1cf9b9085e10488e6ba53b6a934979
Christian Stewart <christian@paral.in>
2020-09-09 01:28:11 -0700

    skiff/core: remove gomod prefix from build target
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/skiff/core/buildroot_ext/package/skiff-core/skiff-core.mk
+++ configs/skiff/core/buildroot_ext/package/skiff-core/skiff-core.mk
@@ -9,7 +9,7 @@ SKIFF_CORE_SITE = $(call github,paralin,skiff-core,$(SKIFF_CORE_VERSION))
 SKIFF_CORE_LICENSE = MIT
 SKIFF_CORE_LICENSE_FILES = LICENSE
 
-SKIFF_CORE_BUILD_TARGETS = github.com/paralin/skiff-core/cmd/skiff-core
+SKIFF_CORE_BUILD_TARGETS = cmd/skiff-core
 
 SKIFF_CORE_TAGS = cgo static_build
 SKIFF_CORE_LDFLAGS = \
