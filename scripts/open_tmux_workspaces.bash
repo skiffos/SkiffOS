fdfe4cb726eaeb4ae3274a96b1ecdfcf68ea816a
Christian Stewart <christian@paral.in>
2020-10-05 04:44:57 -0700

    scripts: add utility to open tmux quickly
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ scripts/open_tmux_workspaces.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -d ./workspaces/ ]; then
+    echo "Run from skiff root."
+    exit 1
+fi
+
+tmux new-session -d -s skiff -n root "cd $(pwd)/skiff; bash -i"
+for wspath in ./skiff/workspaces/*; do
+    wsbase=$(basename $wspath)
+    if [[ $wsbase == .config_* ]]; then
+        continue
+    fi
+    tmux new-window -t 'skiff' -n $wsbase "  cd $(pwd)/skiff; unset SKIFF_CONFIG; export SKIFF_WORKSPACE=$wsbase; bash -i"
+done
+tmux select-window -t 'skiff:0'
+tmux -2 attach-session -t skiff
