6ddca8884bbe69dfbaae872296fca7629e4323a2
Christian Stewart <christian@paral.in>
2020-10-05 04:46:36 -0700

    scripts: fix path to skiff workspaces dir
    Signed-off-by: Christian Stewart <christian@paral.in>

--- scripts/open_tmux_workspaces.bash
+++ scripts/open_tmux_workspaces.bash
@@ -5,13 +5,13 @@ if [ ! -d ./workspaces/ ]; then
     exit 1
 fi
 
-tmux new-session -d -s skiff -n root "cd $(pwd)/skiff; bash -i"
-for wspath in ./skiff/workspaces/*; do
+tmux new-session -d -s skiff -n root "cd $(pwd); bash -i"
+for wspath in ./workspaces/*; do
     wsbase=$(basename $wspath)
     if [[ $wsbase == .config_* ]]; then
         continue
     fi
-    tmux new-window -t 'skiff' -n $wsbase "  cd $(pwd)/skiff; unset SKIFF_CONFIG; export SKIFF_WORKSPACE=$wsbase; bash -i"
+    tmux new-window -t 'skiff' -n $wsbase "  cd $(pwd); unset SKIFF_CONFIG; export SKIFF_WORKSPACE=$wsbase; bash -i"
 done
 tmux select-window -t 'skiff:0'
 tmux -2 attach-session -t skiff
