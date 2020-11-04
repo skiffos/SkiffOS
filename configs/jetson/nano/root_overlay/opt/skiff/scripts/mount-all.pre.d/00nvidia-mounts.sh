fe070b6e944247fc847dc54f5b1b020d8f635d09
Christian Stewart <christian@paral.in>
2020-11-04 12:11:53 -0800

    jetson/nano: specify boot directory as rbind
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/jetson/nano/root_overlay/opt/skiff/scripts/mount-all.pre.d/00nvidia-mounts.sh
+++ configs/jetson/nano/root_overlay/opt/skiff/scripts/mount-all.pre.d/00nvidia-mounts.sh
@@ -2,4 +2,6 @@ export PERSIST_DEVICE="/dev/mmcblk0p1"
 export PERSIST_MNT_FLAGS=""
 export ROOTFS_DEVICE="/dev/mmcblk0p1"
 export ROOTFS_MNT_FLAGS=""
+export BOOT_DEVICE="/mnt/persist/boot"
+export BOOT_MNT_FLAGS="--rbind"
 
