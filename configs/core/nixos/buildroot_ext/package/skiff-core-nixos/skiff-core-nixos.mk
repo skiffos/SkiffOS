21cd32d5372cfed05382da6620444575e3cb32d6
Christian Stewart <christian@paral.in>
2020-11-04 17:06:25 -0800

    skiff-core-nixos: bump version to a33b26
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/core/nixos/buildroot_ext/package/skiff-core-nixos/skiff-core-nixos.mk
+++ configs/core/nixos/buildroot_ext/package/skiff-core-nixos/skiff-core-nixos.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 
-SKIFF_CORE_NIXOS_VERSION = 4b1b1c6c69c9d8c010ea93ae74c6e4db80e0eb5d
+SKIFF_CORE_NIXOS_VERSION = a33b26992e41da7eba079289f503b05b1d6f4844
 SKIFF_CORE_NIXOS_SITE = $(call github,paralin,docker-nixos,$(SKIFF_CORE_NIXOS_VERSION))
 SKIFF_CORE_NIXOS_LICENSE = MIT
 SKIFF_CORE_NIXOS_LICENSE_FILES = LICENSE
