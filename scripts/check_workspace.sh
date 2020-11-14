ad97f2464e786655d296be7c3d24bf542dd412f5
Christian Stewart <christian@paral.in>
2020-11-13 20:27:49 -0800

    .github: full build matrix for target devices
    Signed-off-by: Christian Stewart <christian@paral.in>

--- scripts/check_workspace.sh
+++ scripts/check_workspace.sh
@@ -9,5 +9,7 @@ fi
 
 export SKIFF_FINAL_CONFIG_DIR=$SKIFF_WORKSPACES_DIR/.config_$SKIFF_WORKSPACE/
 export SKIFF_WS_OVERRIDES_DIR=$SKIFF_OVERRIDES_DIR/workspaces/$SKIFF_WORKSPACE/
-export BR2_CCACHE_DIR=${SKIFF_FINAL_CONFIG_DIR}/br2-ccache
+if [ -z "$BR2_CCACHE_DIR" ]; then
+    export BR2_CCACHE_DIR=${SKIFF_FINAL_CONFIG_DIR}/br2-ccache
+fi
 cd - > /dev/null
