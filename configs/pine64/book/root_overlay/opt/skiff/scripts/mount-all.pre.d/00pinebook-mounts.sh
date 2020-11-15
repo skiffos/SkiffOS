92175e5532962913073522b514ccb0ba1784a905
Christian Stewart <christian@paral.in>
2020-11-14 22:43:28 -0800

    pine64/book: remove mmcblk0 requirement due to emmc compat
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/pine64/book/root_overlay/opt/skiff/scripts/mount-all.pre.d/00pinebook-mounts.sh
+++
@@ -1,7 +0,0 @@
-export PERSIST_DEVICE="/dev/mmcblk0p3"
-export PERSIST_MNT_FLAGS=""
-export ROOTFS_DEVICE="/dev/mmcblk0p2"
-export ROOTFS_MNT_FLAGS=""
-export BOOT_DEVICE="/dev/mmcblk0p1"
-export BOOT_MNT_FLAGS=""
-
