c9a1bdf5bc7d2678c6822623bd329470089656bd
Christian Stewart <christian@paral.in>
2021-01-14 10:22:48 -0800

    core/gentoo: use latest stage3, fallback to official images
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ configs/core/gentoo/buildroot_ext/package/skiff-core-gentoo/coreenv/mkoverride.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+VAR=$1
+VAL=$2
+
+if [ ! -f ./overrides.sh ]; then
+    echo "overrides.sh not found"
+    exit 1
+fi
+
+if [ -n "$VAR" ] && [ -n "$VAL" ]; then
+    echo "Setting override ${VAR}=${VAL}..."
+    echo "export ${VAR}=\"${VAL}\"" >> ./overrides.sh
+fi
