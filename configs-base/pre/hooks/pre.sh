de38f09f1d951e4422a95b38137df0d3a9267b94
Christian Stewart <christian@paral.in>
2021-01-20 03:09:01 -0800

    configs-base: restore correct pre-hooks
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs-base/pre/hooks/pre.sh
+++ configs-base/pre/hooks/pre.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
 TARGET_DIR=${SKIFF_BUILDROOT_DIR}/target
+IMAGES_DIR=${SKIFF_BUILDROOT_DIR}/images
+
+mkdir -p ${TARGET_DIR}/etc
+( \
+  echo "NAME=SkiffOS"; \
+  echo "VERSION=${SKIFF_VERSION}"; \
+  echo "ID=skiff"; \
+  echo "VERSION_ID=${SKIFF_VERSION_COMMIT}"; \
+  echo "PRETTY_NAME=\"SkiffOS ${SKIFF_VERSION}\""; \
+  echo "BUILD_DATE=\"$(date)\""; \
+  echo "BUILD_USER=\"${USER}@$(hostname)\""; \
+  echo "VERSION_FULL=\"SkiffOS version ${SKIFF_VERSION} (${USER}@$(hostname)) $(date)\""; \
+  echo "BUILD_CONFIG=\"${SKIFF_CONFIG}\""; \
+  ) > ${TARGET_DIR}/etc/skiff-release
+
+if [ ! -f ${TARGET_DIR}/etc/buildroot-release ]; then
+    cp ${TARGET_DIR}/etc/os-release ${TARGET_DIR}/etc/buildroot-release
+fi
+cp ${TARGET_DIR}/etc/skiff-release ${TARGET_DIR}/etc/os-release
+cp ${TARGET_DIR}/etc/skiff-release ${IMAGES_DIR}/skiff-release
+sed -i '/BUILD_CONFIG/d' ${TARGET_DIR}/etc/os-release
+
+# skiff/systemd
 SYSTEMD_DIR=${TARGET_DIR}/usr/lib/systemd/system
 
 mkdir -p ${SKIFF_BUILDROOT_DIR}/images/resources
