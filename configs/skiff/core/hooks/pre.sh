b680b947281a078688d856c8455c59a6c07b0191
Christian Stewart <christian@paral.in>
2021-01-20 07:44:29 -0800

    skiff/core: remove outdated base config setup hook
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/skiff/core/hooks/pre.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TARGET_DIR=${SKIFF_BUILDROOT_DIR}/target/
-TARGET_COREENV_DIR=$TARGET_DIR/opt/skiff/coreenv
-
-echo "Copying coreenv base..."
-mkdir -p $TARGET_COREENV_DIR/base/
-cat $SKIFF_CURRENT_CONF_DIR/resources/docker/Dockerfile > $TARGET_COREENV_DIR/base/Dockerfile
-cat $SKIFF_CURRENT_CONF_DIR/resources/docker/startup.sh > $TARGET_COREENV_DIR/base/startup.sh
--- configs/skiff/core/resources/docker/Dockerfile
+++
@@ -1,8 +0,0 @@
-# Added by skiff core
-ADD startup.sh /
-RUN chmod 0755 /startup.sh && \
-    touch /.container_startup_in_progress && \
-    mkdir -p /mnt/core
-VOLUME ["/mnt/core"]
-ENTRYPOINT ["/startup.sh"]
-CMD []
