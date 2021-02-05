259de9a19842a0d6623d6b727f4725ae2c54a911
Christian Stewart <christian@paral.in>
2021-02-04 17:06:51 -0800

    skiff/noserial: disable buildroot-console.conf serial-getty
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/skiff/noserial/hooks/pre.sh
+++ configs/skiff/noserial/hooks/pre.sh
@@ -3,8 +3,13 @@
 IMAGES_DIR=${SKIFF_BUILDROOT_DIR}/images
 TARGET_DIR=${SKIFF_BUILDROOT_DIR}/target
 
-echo "Creating disable serial console stamp..."
-touch ${SKIFF_BUILDROOT_DIR}/images/.disable-serial-console
+echo "Disabling serial-getty@ in target..."
+SYSTEMD_DIR=${TARGET_DIR}/usr/lib/systemd/system
+if [ ! -d ${SYSTEMD_DIR} ]; then
+    echo "Could not find systemd dir at ${SYSTEMD_DIR}"
+    exit 1
+fi
+rm -f ${SYSTEMD_DIR}/serial-getty@.service.d/buildroot-console.conf
 
 eval $(cat ${SKIFF_BUILDROOT_DIR}/.config |\
            grep -m1 "BR2_TARGET_GENERIC_GETTY_PORT=" |\
