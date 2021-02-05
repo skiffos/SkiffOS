47b005de7971ab7b4d62e5e062176cf9f10561fc
Christian Stewart <christian@paral.in>
2021-02-04 16:03:38 -0800

    configs/noserial: properly detect getty port
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/skiff/noserial/hooks/pre.sh
+++ configs/skiff/noserial/hooks/pre.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-echo "Creating disable serial console stamp..."
-touch ${SKIFF_WORKSPACE_DIR}/output/images/.disable-serial-console
+IMAGES_DIR=${SKIFF_BUILDROOT_DIR}/images
+TARGET_DIR=${SKIFF_BUILDROOT_DIR}/target
 
+echo "Creating disable serial console stamp..."
+touch ${SKIFF_BUILDROOT_DIR}/images/.disable-serial-console
+
+eval $(cat ${SKIFF_BUILDROOT_DIR}/.config |\
+           grep -m1 "BR2_TARGET_GENERIC_GETTY_PORT=" |\
+           xargs)
 if [ -n "$BR2_TARGET_GENERIC_GETTY_PORT" ] ; then
     echo "Creating systemd mask for ${BR2_TARGET_GENERIC_GETTY_PORT}"
     ln -fs /dev/null "${TARGET_DIR}/etc/systemd/system/serial-getty@${BR2_TARGET_GENERIC_GETTY_PORT}.service"
 fi
-
