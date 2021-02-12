571566bffcb4735feb44e8a3187ae5dcca12c3db
Christian Stewart <christian@paral.in>
2021-02-12 11:16:53 +0000

    skiff/core: bump to version 1.5.4
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/skiff/core/buildroot_ext/package/skiff-core/skiff-core.mk
+++ configs/skiff/core/buildroot_ext/package/skiff-core/skiff-core.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SKIFF_CORE_VERSION = v1.5.3
+SKIFF_CORE_VERSION = v1.5.4
 SKIFF_CORE_SITE = $(call github,paralin,skiff-core,$(SKIFF_CORE_VERSION))
 SKIFF_CORE_LICENSE = MIT
 SKIFF_CORE_LICENSE_FILES = LICENSE
