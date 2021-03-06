fc95ffd31cb5e32696cdc4510f85e9303eb91950
Christian Stewart <christian@paral.in>
2021-03-05 16:55:58 -0800

    skiff-core-linux4tegra: fix build against l4t 32.5.x
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/jetson/common/buildroot_ext/package/skiff-core-linux4tegra/coreenv/patch-debs.bash
+++ configs/jetson/common/buildroot_ext/package/skiff-core-linux4tegra/coreenv/patch-debs.bash
@@ -7,11 +7,21 @@ for dpkgf in /sources/linux4tegra/nv_tegra/l4t_deb_packages/*.deb; do
     fi
     mkdir -p ./workdir
     dpkg-deb -R $dpkgf ./workdir
+    PATCHED=""
     if FILES=$(grep -l -r 'device-tree/compatible' ./workdir/DEBIAN); then
         echo "Patching file $(basename $dpkgf)..."
         sed -i \
             -e 's#/proc/device-tree/compatible#/etc/tegra-soc/device-tree/compatible#g' \
             ${FILES}
+        PATCHED="yes"
+    fi
+    if [[ "$(basename $dpkgf)" == nvidia-l4t-kernel_* ]]; then
+        echo "Removing kernel postinst script..."
+        rm ./workdir/DEBIAN/postinst ./workdir/DEBIAN/postrm
+        PATCHED="yes"
+    fi
+
+    if [ -n "$PATCHED" ]; then
         dpkg-deb -b workdir /sources/l4t_debs_patched/$(basename $dpkgf)
     else
         cp $dpkgf /sources/l4t_debs_patched/$(basename $dpkgf)
