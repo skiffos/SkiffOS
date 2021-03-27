a51b7309cf8c46a4c3fadc3150480728e6b9537d
Christian Stewart <christian@paral.in>
2021-03-27 05:10:52 -0700

    configs-base: enable-swap: set priority to zram over sd
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs-base/pre/root_overlay/opt/skiff/scripts/enable-swap.sh
+++ configs-base/pre/root_overlay/opt/skiff/scripts/enable-swap.sh
@@ -8,8 +8,25 @@ if [ ! -d "/opt/skiff" ]; then
   exit 1
 fi
 
-PERSIST_MNT=/mnt/persist
+# Enable ZRAM if not already enabled.
+# This will compress contents of RAM to avoid using the swapfile.
+SWAP_LIST=$(swapon | cut -d" " -f1 | sed 1d) || true
+ZRAM_SIZE="2048M"
+if ! (echo "${SWAP_LIST}" | grep -q "/dev/zram0"); then
+    echo "Enabling ZRAM at /dev/zram0..."
+    zramctl -s ${ZRAM_SIZE} /dev/zram0 || true
+    swapoff /dev/zram0 2>/dev/null || true
+    mkswap /dev/zram0 || true
+    # set priority to -10
+    if ! swapon -p -10 /dev/zram0 ; then
+        echo "Failed to enable zram0 swap, continuing..."
+    else
+        echo "ZRAM enabled with size ${ZRAM_SIZE}."
+    fi
+fi
 
+# Swap file, in case we run out of RAM.
+PERSIST_MNT=/mnt/persist
 if mountpoint -q $PERSIST_MNT; then
   echo "Found persist drive at $PERSIST_MNT"
 else
@@ -19,7 +36,7 @@ fi
 
 SWAPFILE_PATH=$PERSIST_MNT/primary.swap
 # in mb
-SWAPFILE_SIZE=2000
+SWAPFILE_SIZE=2048
 if [ -f /etc/skiff-swap.env ]; then
     source /etc/skiff-swap.env
 fi
@@ -37,7 +54,8 @@ if [ ! -f $SWAPFILE_PATH ]; then
   dd if=/dev/zero of=$SWAPFILE_PATH bs=1M count=${SWAPFILE_SIZE}
   echo "Done allocating swapfile."
 fi
+
 chmod 600 $SWAPFILE_PATH
 mkswap $SWAPFILE_PATH
-swapon $SWAPFILE_PATH
-
+# set priority to -100
+swapon -p -100 $SWAPFILE_PATH
