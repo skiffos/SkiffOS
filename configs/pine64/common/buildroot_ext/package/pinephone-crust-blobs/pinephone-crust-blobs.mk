0ea8c2b0b75e67730c42f0d2058b87d5b44b2cd9
Christian Stewart <christian@paral.in>
2021-05-27 20:43:39 -0700

    pine64/phone: add support for power-save crust firmware
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ configs/pine64/common/buildroot_ext/package/pinephone-crust-blobs/pinephone-crust-blobs.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# pinephone-crust-blobs
+#
+################################################################################
+
+PINEPHONE_CRUST_BLOBS_VERSION = crust-20210410
+PINEPHONE_CRUST_BLOBS_SITE = $(call github,skiffos,pinephone-crust-blobs,$(PINEPHONE_CRUST_BLOBS_VERSION))
+PINEPHONE_CRUST_BLOBS_LICENSE = BSD-3-Clause, GPL-2.0+
+PINEPHONE_CRUST_BLOBS_LICENSE_FILES = LICENSE.md
+
+PINEPHONE_CRUST_BLOBS_INSTALL_TARGET = NO
+PINEPHONE_CRUST_BLOBS_INSTALL_HOST = NO
+PINEPHONE_CRUST_BLOBS_INSTALL_IMAGES = YES
+
+define PINEPHONE_CRUST_BLOBS_INSTALL_IMAGES_CMDS
+	cp -dpfr $(@D)/bin/. $(BINARIES_DIR)
+endef
+
+$(eval $(generic-package))
