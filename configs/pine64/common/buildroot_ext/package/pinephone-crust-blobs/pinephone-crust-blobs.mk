38aeb03d878e522a91f082d22a02837f4fbaa669
Christian Stewart <christian@paral.in>
2021-11-22 20:22:15 -0800

    pine64/common: update pinephone-crust-blobs to crust-20211124
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/pine64/common/buildroot_ext/package/pinephone-crust-blobs/pinephone-crust-blobs.mk
+++ configs/pine64/common/buildroot_ext/package/pinephone-crust-blobs/pinephone-crust-blobs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PINEPHONE_CRUST_BLOBS_VERSION = crust-20210410
+PINEPHONE_CRUST_BLOBS_VERSION = crust-20211124
 PINEPHONE_CRUST_BLOBS_SITE = $(call github,skiffos,pinephone-crust-blobs,$(PINEPHONE_CRUST_BLOBS_VERSION))
 PINEPHONE_CRUST_BLOBS_LICENSE = BSD-3-Clause, GPL-2.0+
 PINEPHONE_CRUST_BLOBS_LICENSE_FILES = LICENSE.md
@@ -14,7 +14,7 @@ PINEPHONE_CRUST_BLOBS_INSTALL_HOST = NO
 PINEPHONE_CRUST_BLOBS_INSTALL_IMAGES = YES
 
 define PINEPHONE_CRUST_BLOBS_INSTALL_IMAGES_CMDS
-	cp -dpfr $(@D)/bin/. $(BINARIES_DIR)
+	cp -dpfr $(@D)/u-boot-sunxi-with-spl.bin $(BINARIES_DIR)
 endef
 
 $(eval $(generic-package))
