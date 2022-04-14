0a532f01f7ef23d47d358837c190142f26f7d262
Christian Stewart <christian@paral.in>
2022-04-14 12:05:13 -0700

    allwinner/nezha: new configuration layer
    Changes applied:
    
     - u-boot: must change the loading addresses:
       - the kernel for skiffos is too large for defaults
       - the defaults in u-boot expect a ~8MB kernel max.
       - fix: copy arm64 settings to riscv section at configs/sunxi-common.h:207
     - use squashfs boot since the nezha has only 1GB ram
    
    Using a slightly older kernel for now, 5.18.x is available.
    
    Signed-off-by: Christian Stewart <christian@paral.in>

---
+++ configs/allwinner/nezha/root_overlay/opt/skiff/scripts/mount-all.pre.d/00skiff-squashfs-mounts.sh
@@ -0,0 +1,7 @@
+export PERSIST_DEVICE="/dev/mmcblk0p1"
+export PERSIST_RESIZE_DEVICE="/dev/mmcblk0p1"
+export ROOTFS_DEVICE="/mnt/persist/rootfs"
+export ROOTFS_MNT_FLAGS="--rbind"
+export BOOT_DEVICE="/mnt/persist/boot"
+export BOOT_MNT_FLAGS="--rbind"
+export DISABLE_RESIZE_PERSIST="true"
--- configs/allwinner/nezha/scripts/build_image.sh
+++ configs/allwinner/nezha/scripts/build_image.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
-
 if [ $EUID != 0 ]; then
     echo "This script requires sudo, so it might not work."
 fi
 
 set -e
 
-if [ -z "$PI_IMAGE" ]; then
-    echo "Please set PI_IMAGE to the path to the output image."
+if [ -z "$ALLWINNER_IMAGE" ]; then
+    echo "Please set ALLWINNER_IMAGE to the path to the output image."
     exit 1
 fi
 
-if [[ "$PI_IMAGE" != /* ]]; then
+if [[ "$ALLWINNER_IMAGE" != /* ]]; then
     # the "make" command is run from the skiff root,
     # it's most intuitive to take that as the base path
-    PI_IMAGE=$SKIFF_ROOT_DIR/$PI_IMAGE
+    ALLWINNER_IMAGE=$SKIFF_ROOT_DIR/$ALLWINNER_IMAGE
 fi
 
 echo "Allocating sparse image..."
-fallocate -l 1G $PI_IMAGE
+fallocate -l 1G $ALLWINNER_IMAGE
 
 echo "Setting up loopback device..."
-export PI_SD=$(losetup --show -fP $PI_IMAGE)
+export ALLWINNER_SD=$(losetup --show -fP $ALLWINNER_IMAGE)
 function cleanup {
   echo "Removing loopback device..." || true
   sync || true
-  losetup -d $PI_SD || true
+  losetup -d $ALLWINNER_SD || true
 }
 trap cleanup EXIT
 
-if [ -z "${PI_SD}" ] || [ ! -b ${PI_SD} ]; then
+if [ -z "${ALLWINNER_SD}" ] || [ ! -b ${ALLWINNER_SD} ]; then
     echo "Failed to setup loop device."
     exit 1
 fi
 
+
 # Setup no interactive since we know its a brand new file.
 export SKIFF_NO_INTERACTIVE=1
+export DISABLE_CREATE_SWAPFILE=1
 
-echo "Using loopback device at ${PI_SD}"
+echo "Using loopback device at ${ALLWINNER_SD}"
 $SKIFF_CURRENT_CONF_DIR/scripts/format_sd.sh
 $SKIFF_CURRENT_CONF_DIR/scripts/install_sd.sh
-
