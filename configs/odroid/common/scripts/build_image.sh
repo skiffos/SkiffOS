be7eeebca42ec06a8a1ab232eeab89d4d8ba0aaa
Christian Stewart <christian@paral.in>
2022-04-26 09:05:24 -0700

    odroid/common: use newer build-image script
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/odroid/common/scripts/build_image.sh
+++ configs/odroid/common/scripts/build_image.sh
@@ -1,72 +1,43 @@
 #!/bin/bash
-set -eo pipefail
 
-WORK_DIR=`mktemp -d`
-EMPTY_WORK_DIR=`mktemp -d`
-# deletes the temp directory
+if [ $EUID != 0 ]; then
+    echo "This script requires sudo, so it might not work."
+fi
+
+set -e
+
+if [ -z "$ODROID_IMAGE" ]; then
+    echo "Please set ODROID_IMAGE to the path to the output image."
+    exit 1
+fi
+
+if [[ "$ODROID_IMAGE" != /* ]]; then
+    # the "make" command is run from the skiff root,
+    # it's most intuitive to take that as the base path
+    ODROID_IMAGE=$SKIFF_ROOT_DIR/$ODROID_IMAGE
+fi
+
+echo "Allocating sparse image..."
+fallocate -l 1G $ODROID_IMAGE
+
+echo "Setting up loopback device..."
+export ODROID_SD=$(losetup --show -fP $ODROID_IMAGE)
 function cleanup {
-sync || true
-if [ -d "$WORK_DIR" ]; then
-  rm -rf "$WORK_DIR" || true
-fi
-if [ -d "$EMPTY_WORK_DIR" ]; then
-  rm -rf "$EMPTY_WORK_DIR" || true
-fi
+  echo "Removing loopback device..." || true
+  sync || true
+  losetup -d $ODROID_SD || true
 }
 trap cleanup EXIT
 
-OUTPUT_DIR="${SKIFF_BUILDROOT_DIR}/output/images"
-OUTPUT_IMAGE="${OUTPUT_DIR}/sdcard.img"
-OUTPUT_TMP="${OUTPUT_DIR}/tmp.img"
-GENIMAGE_CFG="${SKIFF_CURRENT_CONF_DIR}/resources/gen-image/genimage.cfg"
-
-ubootimg="$BUILDROOT_DIR/output/images/u-boot.bin"
-ubootimgb="$BUILDROOT_DIR/output/images/u-boot-dtb.bin"
-ubootscripts="${BUILDROOT_DIR}/output/images/hk_sd_fuse/"
-
-if [ ! -f $ubootimg ]; then
-  ubootimg=$ubootimgb
+if [ -z "${ODROID_SD}" ] || [ ! -b ${ODROID_SD} ]; then
+    echo "Failed to setup loop device."
+    exit 1
 fi
 
-if [ ! -f $ubootimg ]; then
-  echo "Cannot find u-boot, make sure Buildroot is done compiling."
-  exit 1
-fi
+# Setup no interactive since we know its a brand new file.
+export SKIFF_NO_INTERACTIVE=1
 
-img_path="${OUTPUT_DIR}/Image"
-zimg_path="${OUTPUT_DIR}/zImage"
-dtb_path=$(find ${OUTPUT_DIR}/ -name '*.dtb' -print -quit)
+echo "Using loopback device at ${ODROID_SD}"
+$SKIFF_CURRENT_CONF_DIR/scripts/format_sd.sh
+$SKIFF_CURRENT_CONF_DIR/scripts/install_sd.sh
 
-if [ ! -f "$img_path" ]; then
-  img_path=$zimg_path
-fi
-
-if [ ! -f "$img_path" ]; then
-  echo "zImage or Image not found, make sure Buildroot is done compiling."
-  exit 1
-fi
-
-if [ ! -f "$dtb_path" ]; then
-  echo "dtb not found, make sure Buildroot is done compiling."
-  exit 1
-fi
-
-source ${SKIFF_CURRENT_CONF_DIR}/scripts/determine_config.sh
-
-sed -e "s#BOOT_SCRIPT_NAME#$(basename ${boot_conf})#g" \
-    -e "s#KERNEL_IMAGE_NAME#$(basename ${img_path})#g" \
-    -e "s#DTB_NAME#$(basename ${dtb_path})#g" \
-    ${GENIMAGE_CFG} > ${OUTPUT_DIR}/genimage.cfg
-cp ${boot_conf} ${OUTPUT_DIR}/
-
-genimage \
-  --rootpath "${EMPTY_WORK_DIR}" \
-  --tmppath "${OUTPUT_TMP}" \
-  --inputpath "${OUTPUT_DIR}" \
-  --outputpath "${OUTPUT_DIR}" \
-  --config "${OUTPUT_DIR}/genimage.cfg"
-
-echo "Flashing u-boot..."
-cd $ubootscripts
-SD_FUSE_DD_ARGS="conv=notrunc" ./sd_fusing.sh $OUTPUT_IMAGE $ubootimg
-cd -
