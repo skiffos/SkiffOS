791f38e39f386981db0f3469abf1c02d13103e8c
Christian Stewart <christian@paral.in>
2022-05-07 18:18:54 -0700

    scripts: use different ccache directory outside config dir
    Signed-off-by: Christian Stewart <christian@paral.in>

--- scripts/check_workspace.sh
+++ scripts/check_workspace.sh
@@ -10,6 +10,9 @@ fi
 export SKIFF_FINAL_CONFIG_DIR=$SKIFF_WORKSPACES_DIR/.config_$SKIFF_WORKSPACE/
 export SKIFF_WS_OVERRIDES_DIR=$SKIFF_OVERRIDES_DIR/workspaces/$SKIFF_WORKSPACE/
 if [ -z "$BR2_CCACHE_DIR" ]; then
-    export BR2_CCACHE_DIR=${SKIFF_FINAL_CONFIG_DIR}/br2-ccache
+  export BR2_CCACHE_DIR=${SKIFF_WORKSPACES_DIR}/.ccache/${SKIFF_WORKSPACE}
+  if [ ! -d ${BR2_CCACHE_DIR} ]; then
+    mkdir -p ${BR2_CCACHE_DIR}
+  fi
 fi
 cd - > /dev/null
