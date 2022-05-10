18526cb57e78529c3f961ad2472b7171ce219a11
Christian Stewart <christian@paral.in>
2022-05-09 17:14:41 -0700

    configs-base: cleanup and separate mount-all and docker init
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs-base/pre/root_overlay/opt/skiff/scripts/mount-all.sh
+++ configs-base/pre/root_overlay/opt/skiff/scripts/mount-all.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+BOOT_DEVICE="LABEL=boot"
 PERSIST_DEVICE="LABEL=persist"
 ROOTFS_DEVICE="LABEL=rootfs"
-SYSTEMD_CONFD=/etc/systemd/system
 PERSIST_SUBDIR=/
 PERSIST_MNT=/mnt/persist
+BOOT_MNT=/mnt/boot
 ROOTFS_MNT=/mnt/rootfs
+SYSTEMD_CONFD=/etc/systemd/system
 SKIP_MOUNT_FLAG=/etc/skip-skiff-mounts
 SKIP_JOURNAL_FLAG=/etc/skip-skiff-journal-mounts
 PRE_SCRIPTS_DIR=/opt/skiff/scripts/mount-all.pre.d
@@ -51,85 +53,68 @@ if [ -f $SKIFF_RELEASE_FILE ]; then
     fi
 fi
 
-DOCKER_SERVICE=/usr/lib/systemd/system/docker.service
-DOCKER_CONFD=/etc/systemd/system/docker.service.d
-DOCKER_PERSIST=$SKIFF_PERSIST/docker
-
-# Grab the default docker execstart
-if [ -f $DOCKER_SERVICE ]; then
-    DOCKER_EXECSTART=$(cat $DOCKER_SERVICE | grep '^ExecStart=.*$' | sed -e "s/ExecStart=//")
+# Mount persist device, if applicable.
+mkdir -p $SYSTEMD_CONFD $PERSIST_MNT
+if [ -f $SKIP_MOUNT_FLAG ] || \
+       mountpoint -q $PERSIST_MNT || \
+       mount $PERSIST_MNT_FLAGS $PERSIST_DEVICE $PERSIST_MNT; then
+    echo "Persist drive is at $PERSIST_MNT path $PERSIST_ROOT"
+else
+    echo "Unable to mount ${PERSIST_DEVICE}, continuing."
 fi
 
-mkdir -p \
-      $SYSTEMD_CONFD \
-      $DOCKER_CONFD \
-      $PERSIST_MNT
-if [ -f $SKIP_MOUNT_FLAG ] || mountpoint -q $PERSIST_MNT || mount $PERSIST_MNT_FLAGS $PERSIST_DEVICE $PERSIST_MNT; then
-  echo "Persist drive is at $PERSIST_MNT path $PERSIST_ROOT"
-  mkdir -p \
-        $DOCKER_PERSIST \
-        $JOURNAL_PERSIST \
-        $PERSIST_ROOT/internal \
-        $SKIFF_PERSIST \
-        $SSH_PERSIST
-  if [ -f $DOCKER_SERVICE ]; then
-    echo "Configuring Docker to use $DOCKER_PERSIST"
-    DOCKER_PERSIST=$(realpath ${DOCKER_PERSIST})
-    DOCKER_EXECSTART+=" --data-root=\"$DOCKER_PERSIST\""
+# Create some dirs.
+mkdir -p ${PERSIST_ROOT}/internal ${SKIFF_PERSIST}
 
-    echo "Configuring Docker to use systemd-journald"
-    DOCKER_EXECSTART+=" --log-driver=journald"
-  fi
-
-  if [ ! -f $SKIP_JOURNAL_FLAG ] && ! mountpoint -q /var/log/journal ; then
+# Setup journal persist.
+mkdir -p /var/log/journal ${JOURNAL_PERSIST}
+if [ ! -f $SKIP_JOURNAL_FLAG ] && ! mountpoint -q /var/log/journal ; then
     echo "Configuring systemd-journald to use $JOURNAL_PERSIST"
-    if [ -d /var/log/journal ]; then
-     rm -rf /var/log/journal || true
-    fi
-    mkdir -p /var/log/journal
     mount --bind ${JOURNAL_PERSIST} /var/log/journal
     chmod 4755 /var/log/journal
     systemd-tmpfiles --create --prefix /var/log/journal || true
-  fi
+fi
 
-  if [ ! -f $SSH_PERSIST/sshd_config ]; then
+# Setup ssh persist.
+mkdir -p ${SSH_PERSIST}
+if [ ! -f $SSH_PERSIST/sshd_config ]; then
     cp /etc/ssh/sshd_config $SSH_PERSIST/sshd_config
-  fi
-  if [ ! -f $SSH_PERSIST/ssh_config ]; then
+fi
+if [ ! -f $SSH_PERSIST/ssh_config ]; then
     cp /etc/ssh/ssh_config $SSH_PERSIST/ssh_config
-  fi
-  if ! mountpoint -q /etc/ssh; then
+fi
+if ! mountpoint -q /etc/ssh; then
     mount --bind $SSH_PERSIST /etc/ssh
-  fi
-else
-  echo "Unable to find drive ${PERSIST_DEVICE}!"
-  mkdir -p /var/log/journal
 fi
 
-if [ -n "$ROOTFS_DEVICE_MKDIR" ]; then
-    mkdir -p ${ROOTFS_DEVICE} || echo "Unable to mkdir for rootfs rbind."
-fi
 
-mkdir -p $ROOTFS_MNT
-if [ -f $SKIP_MOUNT_FLAG ] || mountpoint -q $ROOTFS_MNT || mount $ROOTFS_MNT_FLAGS $ROOTFS_DEVICE $ROOTFS_MNT; then
-  echo "Rootfs drive at $ROOTFS_MNT"
-else
-  echo "Unable to find drive ${ROOTFS_DEVICE}!"
-fi
-
+# mount /mnt/boot but only if MOUNT_BOOT_DEVICE is set.
 if [ -n "$BOOT_DEVICE_MKDIR" ]; then
     mkdir -p ${BOOT_DEVICE} || echo "Unable to mkdir for boot rbind."
 fi
-
-if [ -f $DOCKER_SERVICE ]; then
-  echo "Configuring Docker to start with '$DOCKER_EXECSTART'"
-  printf "[Service]\nExecStart=\nExecStart=$DOCKER_EXECSTART" > $DOCKER_CONFD/execstart.conf
+mkdir -p ${BOOT_MNT}
+if [ -f $SKIP_MOUNT_FLAG ] || \
+       [ -z "$MOUNT_BOOT_DEVICE" ] || \
+       mountpoint -q $BOOT_MNT || \
+       mount $BOOT_MNT_FLAGS $BOOT_DEVICE $BOOT_MNT; then
+    echo "Boot device is at ${BOOT_MNT}."
 fi
 
+# Mount rootfs.
+if [ -n "$ROOTFS_DEVICE_MKDIR" ]; then
+    mkdir -p ${ROOTFS_DEVICE} || echo "Unable to mkdir for rootfs rbind."
+fi
+mkdir -p $ROOTFS_MNT
+if [ -f $SKIP_MOUNT_FLAG ] || \
+       mountpoint -q $ROOTFS_MNT || \
+       mount $ROOTFS_MNT_FLAGS $ROOTFS_DEVICE $ROOTFS_MNT; then
+    echo "Rootfs drive is at ${ROOTFS_MNT}."
+fi
+
+# Build SSH keys list.
 echo "Building SSH key list..."
 mkdir -p $KEYS_PERSIST
 echo "Put your SSH keys (*.pub) here." > $KEYS_PERSIST/readme
-
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 touch /root/.ssh/authorized_keys
@@ -137,59 +122,62 @@ chmod 600 /root/.ssh/authorized_keys
 mkdir -p /root/.ssh/tmp-keys
 chmod 700 /root/.ssh/tmp-keys
 if find ${KEYS_PERSIST} -name "*.pub" -mindepth 1 -maxdepth 1 | read; then
-  cp $KEYS_PERSIST/*.pub /root/.ssh/tmp-keys/ 2>/dev/null || true
+    cp $KEYS_PERSIST/*.pub /root/.ssh/tmp-keys/ 2>/dev/null || true
 fi
 if find /etc/skiff/authorized_keys -name "*.pub" -mindepth 1 -maxdepth 1 | read; then
-  cp /etc/skiff/authorized_keys/*.pub /root/.ssh/tmp-keys/ 2>/dev/null || true
+    cp /etc/skiff/authorized_keys/*.pub /root/.ssh/tmp-keys/ 2>/dev/null || true
 fi
 if find /root/.ssh/tmp-keys -name "*.pub" -mindepth 1 -maxdepth 1 | read; then
-  cat /root/.ssh/tmp-keys/*.pub > /root/.ssh/authorized_keys
+    cat /root/.ssh/tmp-keys/*.pub > /root/.ssh/authorized_keys
 else
-  echo "No ssh keys present."
+    echo "No ssh keys present."
 fi
 rm -rf /root/.ssh/tmp-keys
 
+# Setup network manager connections persist.
 mkdir -p /etc/NetworkManager/system-connections
 if ! mountpoint /etc/NetworkManager/system-connections ; then
-  mkdir -p $PERSIST_ROOT/skiff/connections
-  echo "# Place NetworkManager keyfile configs here." > $PERSIST_ROOT/skiff/connections/readme
-  # chmod all files to 0600 or NetworkManager will not read them.
-  chmod 600 ${PERSIST_ROOT}/skiff/connections/*
-  mkdir -p /etc/NetworkManager/system-connections
-  connections_workdir=${SKIFF_OVERLAYS}/nm_connections
-  mkdir -p $connections_workdir
-  mount -t overlay -o lowerdir=/etc/NetworkManager/system-connections,upperdir=${PERSIST_ROOT}/skiff/connections,workdir=$connections_workdir overlay /etc/NetworkManager/system-connections
+    mkdir -p $PERSIST_ROOT/skiff/connections
+    echo "# Place NetworkManager keyfile configs here." > $PERSIST_ROOT/skiff/connections/readme
+    # chmod all files to 0600 or NetworkManager will not read them.
+    chmod 600 ${PERSIST_ROOT}/skiff/connections/*
+    mkdir -p /etc/NetworkManager/system-connections
+    connections_workdir=${SKIFF_OVERLAYS}/nm_connections
+    mkdir -p $connections_workdir
+    mount -t overlay -o lowerdir=/etc/NetworkManager/system-connections,upperdir=${PERSIST_ROOT}/skiff/connections,workdir=$connections_workdir overlay /etc/NetworkManager/system-connections
 fi
 chmod 0755 /etc/NetworkManager
 chmod 0644 /etc/NetworkManager/NetworkManager.conf
 chmod -R 0600 /etc/NetworkManager/system-connections
 chown -R root:root /etc/NetworkManager/system-connections
 
+# Apply any etc overrides.
 if [ -d $PERSIST_ROOT/skiff/etc ]; then
-  rsync --exclude=/readme -rav $PERSIST_ROOT/skiff/etc/ /etc/
+    rsync --exclude=/readme -rav $PERSIST_ROOT/skiff/etc/ /etc/
 else
-  mkdir -p $PERSIST_ROOT/skiff/etc/
+    mkdir -p $PERSIST_ROOT/skiff/etc/
 fi
 echo "Place etc overrides here." > $PERSIST_ROOT/skiff/etc/readme
 
+# Apply hostname from persist partition.
 if [ -f $PERSIST_ROOT/skiff/hostname ] && [ -n "$(cat ${PERSIST_ROOT}/skiff/hostname)" ]; then
-  OHOSTNAME=$(cat /etc/hostname)
-  if [ -z "$OHOSTNAME" ]; then
-      OHOSTNAME=skiff-unknown
-  fi
-  NHOSTNAME=$(cat $PERSIST_ROOT/skiff/hostname)
-  sed -i -e "s/$OHOSTNAME/$NHOSTNAME/g" /etc/hosts
-  echo "$NHOSTNAME" > /etc/hostname
+    OHOSTNAME=$(cat /etc/hostname)
+    if [ -z "$OHOSTNAME" ]; then
+        OHOSTNAME=skiff-unknown
+    fi
+    NHOSTNAME=$(cat $PERSIST_ROOT/skiff/hostname)
+    sed -i -e "s/$OHOSTNAME/$NHOSTNAME/g" /etc/hosts
+    echo "$NHOSTNAME" > /etc/hostname
 
 else
-  hostname > $PERSIST_ROOT/skiff/hostname
-  if [ "$(hostname)" == "" ]; then
-      "skiff-unknown" > $PERSIST_ROOT/skiff/hostname
-  fi
+    hostname > $PERSIST_ROOT/skiff/hostname
+    if [ "$(hostname)" == "" ]; then
+        "skiff-unknown" > $PERSIST_ROOT/skiff/hostname
+    fi
 fi
 
-systemctl daemon-reload
 hostname -F /etc/hostname # change transient hostname
+systemctl daemon-reload
 
 # Attempt to resize disk, if necessary.
 # Note: this is an online resize, no re-mount required.
@@ -208,4 +196,5 @@ for i in ${EXTRA_SCRIPTS_DIR}/*.sh ; do
 done
 
 # Probe udev to make sure any new kernel modules are picked up.
+systemctl daemon-reload
 systemctl restart --no-block systemd-udev-trigger.service || true
---
+++ configs/apps/docker/root_overlay/opt/skiff/scripts/mount-all.d/02-skiff-apps-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DOCKER_SERVICE=/usr/lib/systemd/system/docker.service
+DOCKER_CONFD=/etc/systemd/system/docker.service.d
+DOCKER_PERSIST=${SKIFF_PERSIST}/docker
+
+# Grab the default docker execstart
+mkdir -p ${DOCKER_CONFD}
+if [ -f $DOCKER_SERVICE ]; then
+    DOCKER_EXECSTART=$(cat $DOCKER_SERVICE | grep '^ExecStart=.*$' | sed -e "s/ExecStart=//")
+fi
+
+# Setup Docker mount, if applicable.
+if [ -f $DOCKER_SERVICE ]; then
+    echo "Configuring Docker to use $DOCKER_PERSIST"
+    DOCKER_PERSIST=$(realpath ${DOCKER_PERSIST})
+    mkdir -p ${DOCKER_PERSIST}
+    DOCKER_EXECSTART+=" --data-root=\"$DOCKER_PERSIST\""
+
+    echo "Configuring Docker to use systemd-journald"
+    DOCKER_EXECSTART+=" --log-driver=journald"
+
+    echo "Configuring Docker to start with '$DOCKER_EXECSTART'"
+    printf "[Service]\nExecStart=\nExecStart=$DOCKER_EXECSTART" > $DOCKER_CONFD/execstart.conf
+fi
