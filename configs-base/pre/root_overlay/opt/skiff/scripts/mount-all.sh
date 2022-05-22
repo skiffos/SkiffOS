961b4fc4f1ec2d61bfe71a5ae6542e838de1ecf8
Christian Stewart <christian@paral.in>
2022-05-21 18:06:27 -0700

    configs-base: force resize2fs to perform online resize of /
    Fixes #202
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs-base/pre/root_overlay/opt/skiff/scripts/mount-all.sh
+++ configs-base/pre/root_overlay/opt/skiff/scripts/mount-all.sh
@@ -189,13 +189,35 @@ if [ -n "${DISABLE_RESIZE_PARTITION}" ]; then
 fi
 if [ -z "${DISABLE_RESIZE_PERSIST}" ]; then
     echo "Resizing ${PERSIST_MNT} with embiggen-disk..."
-    if ! ${RESIZE_DISK[@]} ${PERSIST_MNT}; then
-        # This will only work with /dev/ paths.
-        if [ -b ${PERSIST_DEVICE} ]; then
-            echo "Failed to resize via mountpoint, trying via ${PERSIST_DEVICE}..."
-            ${RESIZE_DISK[@]} ${PERSIST_DEVICE} || echo "Failed to resize persist. Continuing anyway."
+    # This will only work with /dev/ paths.
+    if [ -b ${PERSIST_DEVICE} ]; then
+        # Sometimes the path does not appear in the mounts list.
+        #
+        # Workaround: temporarily mount it so resize2fs realizes it needs to
+        # do an online resize. Otherwise returns an error.
+        ROOT_MOUNT_SOURCE=$(findmnt --target / --first-only -n -o "SOURCE")
+        if [ -n "${FORCE_RESIZE_PERSIST_MOUNT}" ] || \
+               [[ "${ROOT_MOUNT_SOURCE}" != "${PERSIST_DEVICE}" ]] || \
+               ! findmnt --source ${PERSIST_DEVICE} --first-only; then
+            echo "Temporarily mounting ${PERSIST_DEVICE} to force online resize..."
+            RESIZE_DISK_MTPT=/mnt/.persist-resize
+            mkdir -p ${RESIZE_DISK_MTPT}
+            mount ${PERSIST_DEVICE} ${RESIZE_DISK_MTPT} || true
+        fi
+        echo "Resizing with embiggen-disk via ${PERSIST_DEVICE}..."
+        if ${RESIZE_DISK[@]} ${PERSIST_DEVICE}; then
+            PERSIST_RESIZED="true"
+        fi
+        if [ -n "${RESIZE_DISK_MTPT}" ]; then
+            echo "Unmounting ${RESIZE_DISK_MTPT}..."
+            umount ${RESIZE_DISK_MTPT} || true
+        fi
+    fi
+    if [ -z "${PERSIST_RESIZED}" ]; then
+        if ! ${RESIZE_DISK[@]} ${PERSIST_MNT}; then
+            echo "Failed to resize persist, continuing anyway."
         else
-            echo "Failed to resize persist via mountpoint and via device path. Continuing anyway."
+            PERSIST_RESIZED="true"
         fi
     fi
 fi
