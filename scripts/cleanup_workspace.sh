f2f2902546c4b5b2dc6ceaaf25f479c893c92968
Christian Stewart <christian@paral.in>
2022-05-21 17:02:51 -0700

    clean: add option to clear compiler cache
    Signed-off-by: Christian Stewart <christian@paral.in>

--- scripts/cleanup_workspace.sh
+++ scripts/cleanup_workspace.sh
@@ -13,6 +13,15 @@ if [ -z "$SKIFF_NO_INTERACTIVE" ]; then
   fi
 fi
 
+
+if [ -z "$SKIFF_KEEP_CCACHE" ] && [ -z "$SKIFF_NO_INTERACTIVE" ]; then
+    read -p "Do you want to also clear the compiler cache? [y/N] " -n 1 -r
+    echo    # (optional) move to a new line
+    if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+        export SKIFF_KEEP_CCACHE="true"
+    fi
+fi
+
 if [ "$SKIFF_WORKSPACE" != "default" ] ; then
   rm -rf $WORKSPACE_DIR
 else
@@ -24,4 +33,9 @@ fi
 # Delete the config dir
 if [ -d "$SKIFF_FINAL_CONFIG_DIR" ]; then
   rm -rf $SKIFF_FINAL_CONFIG_DIR
+fi
+
+# Delete the compiler cache.
+if [ -z "$SKIFF_KEEP_CCACHE" ] && [ -d "$BR2_CCACHE_DIR" ]; then
+    rm -rf ${BR2_CCACHE_DIR}
 fi
