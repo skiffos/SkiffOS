864b85d5ea1b191621cd3e270c474bd567dc6463
Christian Stewart <christian@paral.in>
2022-08-23 02:03:54 -0700

    apps/podman: symlink /var/lib/containers to persist
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/apps/podman/root_overlay/opt/skiff/scripts/mount-all.d/02-skiff-apps-podman.sh
+++ configs/apps/podman/root_overlay/opt/skiff/scripts/mount-all.d/02-skiff-apps-podman.sh
@@ -3,6 +3,10 @@
 PODMAN_SERVICE=/usr/lib/systemd/system/podman.service
 PODMAN_CONFD=/etc/systemd/system/podman.service.d
 PODMAN_PERSIST=${SKIFF_PERSIST}/podman
+PODMAN_PERSIST=$(realpath ${PODMAN_PERSIST})
+
+# make podman persist dir
+mkdir -p ${PODMAN_PERSIST}
 
 # Grab the default podman execstart
 mkdir -p ${PODMAN_CONFD}
@@ -10,11 +14,17 @@ if [ -f $PODMAN_SERVICE ]; then
     PODMAN_EXECSTART=$(cat $PODMAN_SERVICE | grep '^ExecStart=.*$' | sed -e "s/ExecStart=//")
 fi
 
+# If /var/lib/containers exists, remove it.
+if [ -e /var/lib/containers ]; then
+    rm -rf /var/lib/containers
+fi
+
+# Symlink /var/lib/containers to /mnt/persist/skiff/podman
+ln -fs ${PODMAN_PERSIST} /var/lib/containers
+
 # Setup Podman mount, if applicable.
 if [ -f $PODMAN_SERVICE ]; then
     echo "Configuring Podman to use $PODMAN_PERSIST"
-    PODMAN_PERSIST=$(realpath ${PODMAN_PERSIST})
-    mkdir -p ${PODMAN_PERSIST}
     PODMAN_EXECSTART+=" --root=\"$PODMAN_PERSIST\""
 
     echo "Configuring Podman to use crun"
