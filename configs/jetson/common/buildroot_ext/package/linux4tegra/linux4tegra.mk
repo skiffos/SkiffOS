3f5b9ddf00de936a95dfdf5d6298fd51f881dd48
Christian Stewart <christian@paral.in>
2022-09-02 21:25:55 -0700

    linux4tegra: fix indentation in makefile
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/jetson/common/buildroot_ext/package/linux4tegra/linux4tegra.mk
+++ configs/jetson/common/buildroot_ext/package/linux4tegra/linux4tegra.mk
@@ -23,41 +23,41 @@ LINUX4TEGRA_LICENSE_FILES = \
 	bootloader/LICENSE.mkgpt \
 	bootloader/LICENSE.mksparse \
 	bootloader/LICENSE.tegraopenssl \
-  nv_tegra/nvidia_configs/opt/nvidia/l4t-usb-device-mode/LICENSE.filesystem.img \
-  nv_tegra/LICENSE.libtegrav4l2 \
-  nv_tegra/LICENSE.libnvcam_imageencoder \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libvulkan1 \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnvargus \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnvv4lconvert \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libtegrav4l2 \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnvv4l2 \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.nvdla \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.cypress_wifibt \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.minigbm \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnvjpeg \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.brcm_patchram_plus \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnvcam_imageencoder \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnvtracebuf \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.realtek_8822ce_wifibt \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.tegra_sensors \
-  nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnveventlib \
-  nv_tegra/LICENSE.libnveventlib \
-  nv_tegra/LICENSE.libnvscf \
-  nv_tegra/LICENSE.libnvargus \
-  nv_tegra/LICENSE.minigbm \
-  nv_tegra/LICENSE.wayland-ivi-extension \
-  nv_tegra/nv_sample_apps/LICENSE.gst-nvvideo4linux2 \
-  nv_tegra/nv_sample_apps/LICENSE.gstvideocuda \
-  nv_tegra/nv_sample_apps/LICENSE.gst-openmax \
-  nv_tegra/nv_sample_apps/LICENSE.libgstnvdrmvideosink \
-  nv_tegra/nv_sample_apps/LICENSE.libgstnvvideosinks \
-  nv_tegra/LICENSE \
-  nv_tegra/LICENSE.weston-data \
-  nv_tegra/LICENSE.libnvtracebuf \
-  nv_tegra/LICENSE.weston \
-  nv_tegra/LICENSE.nvdla \
-  nv_tegra/LICENSE.l4t-usb-device-mode-filesystem.img \
-  nv_tegra/LICENSE.brcm_patchram_plus
+	nv_tegra/nvidia_configs/opt/nvidia/l4t-usb-device-mode/LICENSE.filesystem.img \
+	nv_tegra/LICENSE.libtegrav4l2 \
+	nv_tegra/LICENSE.libnvcam_imageencoder \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libvulkan1 \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnvargus \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnvv4lconvert \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libtegrav4l2 \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnvv4l2 \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.nvdla \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.cypress_wifibt \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.minigbm \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnvjpeg \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.brcm_patchram_plus \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnvcam_imageencoder \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnvtracebuf \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.realtek_8822ce_wifibt \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.tegra_sensors \
+	nv_tegra/nvidia_drivers/usr/share/doc/nvidia-tegra/LICENSE.libnveventlib \
+	nv_tegra/LICENSE.libnveventlib \
+	nv_tegra/LICENSE.libnvscf \
+	nv_tegra/LICENSE.libnvargus \
+	nv_tegra/LICENSE.minigbm \
+	nv_tegra/LICENSE.wayland-ivi-extension \
+	nv_tegra/nv_sample_apps/LICENSE.gst-nvvideo4linux2 \
+	nv_tegra/nv_sample_apps/LICENSE.gstvideocuda \
+	nv_tegra/nv_sample_apps/LICENSE.gst-openmax \
+	nv_tegra/nv_sample_apps/LICENSE.libgstnvdrmvideosink \
+	nv_tegra/nv_sample_apps/LICENSE.libgstnvvideosinks \
+	nv_tegra/LICENSE \
+	nv_tegra/LICENSE.weston-data \
+	nv_tegra/LICENSE.libnvtracebuf \
+	nv_tegra/LICENSE.weston \
+	nv_tegra/LICENSE.nvdla \
+	nv_tegra/LICENSE.l4t-usb-device-mode-filesystem.img \
+	nv_tegra/LICENSE.brcm_patchram_plus
 
 LINUX4TEGRA_INSTALL_IMAGES = YES
 
