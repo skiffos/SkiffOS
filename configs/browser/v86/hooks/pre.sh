e6ac217ae2bf2fe632370c8990e6f4874544c0ec
Christian Stewart <christian@paral.in>
2022-10-05 12:04:56 -0700

    browser/v86: use HDA instead of 9p for container storage
    Fixes #247
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/browser/v86/hooks/pre.sh
+++ configs/browser/v86/hooks/pre.sh
@@ -2,9 +2,7 @@
 set -eo pipefail
 
 TARGET_DIR=${SKIFF_BUILDROOT_DIR}/target
-if [ ! -d ${TARGET_DIR}/boot ]; then
-    mkdir ${TARGET_DIR}/boot
-fi
+mkdir -p ${TARGET_DIR}/boot ${TARGET_DIR}/persist
 
 # Mask systemd-sysupdate.service
 ln -fs /dev/null "${TARGET_DIR}/etc/systemd/system/systemd-sysupdate.service"
--- configs/browser/v86/root_overlay/opt/skiff/scripts/mount-all.pre.d/00skiff-v86-mounts.sh
+++ configs/browser/v86/root_overlay/opt/skiff/scripts/mount-all.pre.d/00skiff-v86-mounts.sh
@@ -3,6 +3,10 @@ export BOOT_MNT_FLAGS="--rbind"
 export MOUNT_BOOT_DEVICE="true"
 export ROOTFS_DEVICE="/boot"
 export ROOTFS_MNT_FLAGS="--rbind"
-export PERSIST_DEVICE="host9p"
-export PERSIST_MNT_FLAGS="-o trans=virtio,rw,cache=fscache -t 9p"
+export PERSIST_DEVICE="/persist"
+export PERSIST_MNT_FLAGS="--rbind"
 export DISABLE_RESIZE_PERSIST="true"
+
+# To use the 9p filesystem instead:
+# export PERSIST_DEVICE="host9p"
+# export PERSIST_MNT_FLAGS="-o trans=virtio,rw,cache=fscache -t 9p"
