a8a91c8abb93f202aca7f4569d8daf85faf83309
Christian Stewart <christian@paral.in>
2022-10-05 11:29:00 -0700

    browser/v86: mask systemd-sysupdate.service
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/browser/v86/hooks/pre.sh
+++ configs/browser/v86/hooks/pre.sh
@@ -5,3 +5,6 @@ TARGET_DIR=${SKIFF_BUILDROOT_DIR}/target
 if [ ! -d ${TARGET_DIR}/boot ]; then
     mkdir ${TARGET_DIR}/boot
 fi
+
+# Mask systemd-sysupdate.service
+ln -fs /dev/null "${TARGET_DIR}/etc/systemd/system/systemd-sysupdate.service"
