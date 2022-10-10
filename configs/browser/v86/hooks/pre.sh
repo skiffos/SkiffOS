e2fbaaab905395faacd23599b82c63fb3cc80309
Christian Stewart <christian@paral.in>
2022-10-10 14:03:56 -0700

    browser/v86: cmdline args and cleanup pre hook
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/browser/v86/hooks/pre.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-TARGET_DIR=${SKIFF_BUILDROOT_DIR}/target
-mkdir -p ${TARGET_DIR}/boot ${TARGET_DIR}/persist
-
-# Mask systemd-sysupdate.service
-ln -fs /dev/null "${TARGET_DIR}/etc/systemd/system/systemd-sysupdate.service"
