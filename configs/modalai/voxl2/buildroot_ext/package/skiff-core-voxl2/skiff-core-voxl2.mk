24b6e1f1e46dd0c653071a42c38a4d51b6ee0ad6
Christian Stewart <christian@paral.in>
2022-10-26 11:41:33 -0700

    modalai/voxl2: add voxl2-import-core.sh utility script
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/modalai/voxl2/buildroot_ext/package/skiff-core-voxl2/skiff-core-voxl2.mk
+++ configs/modalai/voxl2/buildroot_ext/package/skiff-core-voxl2/skiff-core-voxl2.mk
@@ -11,6 +11,8 @@ define SKIFF_CORE_VOXL2_INSTALL_COREENV
 	cp -r $(SKIFF_CORE_VOXL2_PKGDIR)/coreenv/* $(TARGET_DIR)/opt/skiff/coreenv/skiff-core-voxl2/
 	$(INSTALL) -m 0644 $(SKIFF_CORE_VOXL2_PKGDIR)/coreenv-defconfig.yaml \
 		$(TARGET_DIR)/opt/skiff/coreenv/defconfig.yaml
+	$(INSTALL) -m 0755 $(SKIFF_CORE_VOXL2_PKGDIR)/voxl2-import-core.sh \
+		$(TARGET_DIR)/usr/bin/voxl2-import-core.sh
 	touch $(TARGET_DIR)/opt/skiff/coreenv/.overridden
 endef
 
