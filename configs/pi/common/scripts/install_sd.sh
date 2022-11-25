607f01183fea9044b24bc9662a2c8a5b31abbc42
Christian Stewart <christian@paral.in>
2022-11-25 00:45:41 -0800

    pi/0: use skiff-init-squashfs to save memory
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/pi/common/scripts/install_sd.sh
+++ configs/pi/common/scripts/install_sd.sh
@@ -28,6 +28,8 @@ fi
 outp_path="${BUILDROOT_DIR}/output"
 uimg_path="${outp_path}/images/zImage"
 cpio_path="${outp_path}/images/rootfs.cpio.lz4"
+squashfs_path="${outp_path}/images/rootfs.squashfs"
+skiff_init_path="${outp_path}/images/skiff-init/"
 firm_path="${outp_path}/images/rpi-firmware"
 
 if [ ! -f "$uimg_path" ]; then
@@ -92,9 +94,9 @@ cp $pi_cmdline_txt $boot_dir/cmdline.txt
 sync
 
 if [ -d "$outp_path/images/boot_part" ]; then
-    echo "Copying boot_part..."
-    ${RS} $outp_path/images/boot_part/ $boot_dir/
-    sync
+  echo "Copying boot_part..."
+  ${RS} $outp_path/images/boot_part/ $boot_dir/
+  sync
 fi
 
 if [ -d "$outp_path/images/rootfs_part" ]; then
@@ -105,7 +107,7 @@ fi
 
 if [ -d "$outp_path/images/persist_part" ]; then
   echo "Copying persist_part..."
-   ${RS} $outp_path/images/persist_part/ $persist_dir/
+  ${RS} $outp_path/images/persist_part/ $persist_dir/
   sync
 fi
 
@@ -113,8 +115,22 @@ echo "Copying device tree(s)..."
 ${RS} $outp_path/images/*.dtb $boot_dir/
 sync
 
-echo "Copying uInitrd..."
-${RS} $cpio_path $boot_dir/rootfs.cpio.lz4
-sync
+if [ -f $cpio_path ]; then
+  echo "Copying rootfs.cpio.lz4..."
+  ${RS} $cpio_path $boot_dir/rootfs.cpio.lz4
+  sync
+fi
+
+if [ -f $squashfs_path ]; then
+  echo "Copying rootfs.squashfs..."
+  ${RS} $squashfs_path $boot_dir/rootfs.squashfs
+  sync
+fi
+
+if [ -d $skiff_init_path ]; then
+  echo "Copying skiff-init..."
+  ${RS} $skiff_init_path $boot_dir/skiff-init/
+  sync
+fi
 
 cleanup
