de35010f7c4e61ac1f328052b8d0ca67d92341f3
Christian Stewart <christian@aperture.us>
2023-05-11 18:39:19 -0700

    apps/crio: fix to path in systemd file
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- configs/apps/crio/root_overlay/opt/skiff/scripts/mount-all.d/02-skiff-apps-crio.sh
+++ configs/apps/crio/root_overlay/opt/skiff/scripts/mount-all.d/02-skiff-apps-crio.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Override /var/lib/containers
 CONTAINERS_PERSIST=${CONTAINERS_PERSIST:=${SKIFF_PERSIST}/containers}
 CONTAINERS_PERSIST=$(realpath ${CONTAINERS_PERSIST})
 
@@ -13,3 +14,18 @@ fi
 
 # Symlink /var/lib/containers to /mnt/persist/skiff/containers
 ln -fs ${CONTAINERS_PERSIST} /var/lib/containers
+
+# Override /var/lib/crio
+CRIO_PERSIST=${CRIO_PERSIST:=${SKIFF_PERSIST}/crio}
+CRIO_PERSIST=$(realpath ${CRIO_PERSIST})
+
+# Make crio persist dir
+mkdir -p ${CRIO_PERSIST}
+
+# If /var/lib/crio exists, remove it.
+if [ -e /var/lib/crio ]; then
+    rm -rf /var/lib/crio
+fi
+
+# Symlink /var/lib/crio to /mnt/persist/skiff/crio
+ln -fs ${CRIO_PERSIST} /var/lib/crio
