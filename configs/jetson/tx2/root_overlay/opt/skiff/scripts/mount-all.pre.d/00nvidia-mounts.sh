14c3fcb3573c9549be107ff9e0c3d99a45db8f23
Christian Stewart <christian@aperture.us>
2023-12-27 18:11:32 -0800

    jetson/tx2: no resize persist, adjust serial port
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- configs/jetson/tx2/root_overlay/opt/skiff/scripts/mount-all.pre.d/00nvidia-mounts.sh
+++ configs/jetson/tx2/root_overlay/opt/skiff/scripts/mount-all.pre.d/00nvidia-mounts.sh
@@ -4,3 +4,6 @@ export ROOTFS_MNT_FLAGS="--rbind"
 export BOOT_DEVICE="/mnt/persist/boot"
 export BOOT_DEVICE_MKDIR="true"
 export BOOT_MNT_FLAGS="--rbind"
+
+# resized by skiff-init-squashfs
+export DISABLE_RESIZE_PERSIST="true"
