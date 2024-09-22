edf302de3e51b3de757cba51abd12771c2340fa0
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2024-09-22 15:42:25 +0200

    virt/incus: Build incus image inside an incus container

--- configs/virt/incus/scripts/buildimage.sh
+++ configs/virt/incus/scripts/buildimage.sh
@@ -3,17 +3,34 @@ set -eu
 
 IMAGE_NAME=skiffos/testing
 CONTAINER=skiff
-OUTPUT_PATH="$BUILDROOT_DIR/output"
-IMAGES_PATH="$BUILDROOT_DIR/images"
-INCUS_IMAGE_PATH="${IMAGES_PATH}/incus.tar.gz"
-roottar="${IMAGES_PATH}/rootfs.tar"
+#IMAGES_PATH="$BUILDROOT_DIR/images"
 
-TMPDIR="${BUILDROOT_DIR}/incus-tmp"
-trap 'rm -rf "$TMPDIR";' EXIT
-rm -rf "$TMPDIR"
-mkdir -p "${TMPDIR}/rootfs"
-tar -xf "$roottar" -C "$TMPDIR/rootfs"
-pushd "$TMPDIR"
+if ! command -v incus >/dev/null 2>&1; then
+	echo "Failed to find the incus command. Is incus installed on your host system?"
+	exit 1
+fi
+
+# create utility container
+echo "Creating builder container..."
+trap 'incus rm -f skiff-builder' EXIT
+incus create images:alpine/3.20 skiff-builder
+incus config device add skiff-builder buildsource disk shift=true source="${BUILDROOT_DIR}" path="/mnt/build"
+incus start skiff-builder
+for i in {1..10}
+do
+  echo "Install container dependencies (attempt $i/10)..."
+  if incus exec skiff-builder -- apk add tar bash
+  then
+    break
+  elif [[ "$i" == 10 ]]
+  then
+    echo "Failed to install container dependencies"
+    exit 1
+  fi
+  sleep 3
+done
+echo "done."
+
 creationtime="$(date +%s)"
 default_metadata="architecture: x86_64
 creation_date: $creationtime
@@ -21,8 +38,30 @@ properties:
   description: SkiffOS
   os: Linux x86
   release: unknown"
-echo "${INCUS_METADATA:-"${default_metadata}"}" >"${TMPDIR}/metadata.yaml"
-tar caf "${INCUS_IMAGE_PATH}" rootfs metadata.yaml
-popd
+
+TMPDIR_HOST="${BUILDROOT_DIR}/incus-tmp"
+touch "${BUILDROOT_DIR}/uid_test"
+trap 'rm "${BUILDROOT_DIR}/uid_test"; incus rm -f skiff-builder' EXIT
+
+incus exec skiff-builder -- bash <<EOF
+set -eux
+
+OUTPUT_PATH="/mnt/build/output"
+IMAGES_PATH="/mnt/build/images"
+INCUS_IMAGE_PATH="\${IMAGES_PATH}/incus.tar.gz"
+roottar="\${IMAGES_PATH}/rootfs.tar"
+TMPDIR="/tmp/skiff/incus"
+
+rm -rf "\$TMPDIR"
+trap 'rm -rf \${TMPDIR}' EXIT
+mkdir -p "\${TMPDIR}/rootfs"
+tar --same-owner -xf "\$roottar" -C "\${TMPDIR}/rootfs"
+cd "\$TMPDIR"
+echo "${INCUS_METADATA:-"${default_metadata}"}" > "\${TMPDIR}/metadata.yaml"
+tar caf "\${INCUS_IMAGE_PATH}" rootfs metadata.yaml
+host_user_id="\$(stat -c '%u' "/mnt/build/uid_test")"
+host_group_id="\$(stat -c '%g' "/mnt/build/uid_test")"
+chown "\${host_user_id}:\${host_group_id}" "\${INCUS_IMAGE_PATH}"
+EOF
 
 echo "Incus image generated successfully as ${IMAGE_NAME}"
